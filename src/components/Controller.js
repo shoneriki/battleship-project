fbfc2863486072d326b29b88e4901cc08c8dac32
Sho Neriki <ikirenohs@gmail.com>
2023-03-19 15:34:13 +0900

    comments for preliminary game logic

---
+++ src/components/Controller.js
@@ -0,0 +1,24 @@
+// import ship later
+// import gameboard later
+// import player later
+
+class Controller {
+  // prep stage
+    // two players
+    // each player has a gameboard
+    // 5 ships (length 2,3,3,4,5) on each gameboard placed
+    // function to check that all ships are placed
+
+  // game stage
+    // whilst all ships of either player are not sunk
+      // player 1 attacks player 2
+      // player 2 attacks player 1
+
+  // end game
+    // display winner and loser
+    // loser is determined when all ships are sunk
+    // winner is determined by not being loser
+
+
+
+}
