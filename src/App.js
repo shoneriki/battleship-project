726ac56eedb90b36e6480e86300b16a64b77c76f
Sho Neriki <ikirenohs@gmail.com>
2023-03-22 10:49:16 +0900

    boards responsiveness

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
 import Gameboard from "./components/Gameboard"
+import styled from "styled-components"
+
+const AppSection = styled.main`
+  width: 100%;
+  min-height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media only screen and (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media only screen and (min-width: 801px) and  (max-width: 1100px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+`;
+const Title = styled.h1`
+
+`
+
+const Boards = styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  text-align: center;
+  @media only screen and (max-width: 1100px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    min-height: 30vh;
+  }
+`;
+const GameTitle = styled.h1`
+  text-align: center;
+`;
+
+const BoardSection = styled.section`
+  width: 50%;
+  height: 100%;
+  border-radius: 10px;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className="app">
-      <h1 className="title">Battleship</h1>
-      <div className="boards">
-        <div className="board">
-          <h2>Player</h2>
-          <Gameboard/>
-        </div>
-        <div className="board">
-          <h2>Computer Board</h2>
-          <Gameboard/>
-        </div>
-      </div>
-    </div>
+    <AppSection>
+      <GameTitle>Battleship</GameTitle>
+      <Boards>
+        <BoardSection>
+          <Gameboard
+            Player="Player 1"
+          />
+        </BoardSection>
+        <BoardSection>
+          <Gameboard
+            Player="Computer"
+          />
+        </BoardSection>
+      </Boards>
+    </AppSection>
 
   );
 }
--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,61 +1,88 @@
 import styled from "styled-components"
 import {GameboardConstructor} from "./GameboardConstructor"
-const Gameboard = () => {
+const Gameboard = ({Player}) => {
   const {
     board
   } = GameboardConstructor()
 
+  const PlayerTitle = styled.h1`
+    text-align: center;
+  `;
+
   const Board = styled.table`
-    width: 80%;
+    width: 100%;
     min-height: 100vh;
-    margin: auto;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
-
-  `
+    margin-top: 10%;
+    @media only screen and (max-width: 1300px) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 30vh;
+      width: 100%;
+    }
+  `;
 
   const TableRow = styled.tr`
     display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    height: 25%;
+    width: 50%;
+    height: 15%;
+    @media only screen and (max-width: 800px) {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    @media only screen and (min-width: 801px) and  (max-width: 1100px) {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   `
   const Square = styled.td`
     border: 1px solid black;
-    width: 10%;
+    width: calc(10%);
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100%;
-    padding: 32px;
+    height: 10%;
+    padding: 16px;
     font-size: 8px;
+
+    @media only screen and (max-width: 800px) {
+      width: calc(10% - 8px)
+    }
+    @media only screen and (min-width: 801px) and  (max-width: 1100px) {
+      width: calc(10% - 64px)
+    }
+
   `
   // v for vertical, h for horizontal
   return (
-      <Board>
-        {board.map((row, v) => (
-          <TableRow key={v}>
-            {row.map((cell, h) => (
-              <Square
-                key={`${v}, ${h}`}
-                v={cell.v}
-                h={cell.h}
-                hasShip={cell.hasShip}
-                data-testid={`cell-${v}-${h}`}
-                backgroundColor={cell.hasShip ? "red" : "white"}
-                style={{
-                  backgroundColor: cell.hasShip ? "red" : "blue",
-                }}
-                onHover={{backgroundColor: "blue"}}
-              >
-
-              </Square>
-            ))}
-          </TableRow>
-        ))}
-      </Board>
+    <Board>
+      <PlayerTitle>{Player}</PlayerTitle>
+      {board.map((row, v) => (
+        <TableRow key={v}>
+          {row.map((cell, h) => (
+            <Square
+              key={`${v}, ${h}`}
+              v={cell.v}
+              h={cell.h}
+              hasShip={cell.hasShip}
+              data-testid={`cell-${v}-${h}`}
+              backgroundColor={cell.hasShip ? "red" : "white"}
+              style={{
+                backgroundColor: cell.hasShip ? "red" : "blue",
+              }}
+              onHover={{ backgroundColor: "blue" }}
+            ></Square>
+          ))}
+        </TableRow>
+      ))}
+    </Board>
   );
 }
 export default Gameboard
