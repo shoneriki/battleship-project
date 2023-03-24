74f7d84cce3585093c7ffa58623e3b7c1acd84e0
Sho Neriki <34837760+shoneriki@users.noreply.github.com>
2023-03-24 15:33:05 +0900

    when all ships are placed, shipInfo disappears (#8)

--- src/__tests__/PlayerArea.test.js
+++ src/__tests__/PlayerArea.test.js
@@ -1,4 +1,4 @@
-import { render, screen, getByTestId, fireEvent, waitFor} from "@testing-library/react";
+import { render, screen, getByTestId, fireEvent, waitFor, waitForElementToBeRemoved} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom/extend-expect'
@@ -27,6 +27,55 @@ test("ship can be placed", async () => {
   })
 })
 
+test("when all ships are placed, ships can't be placed", async () => {
+  render(<AppSection/>)
+  const square = screen.getByTestId("Player-cell-0-0")
+  const playerInfo = screen.getByTestId("Player-ship-info");
+  expect(getComputedStyle(square).getPropertyValue("background-color")).not.toBe("green")
+  act(() => {
+    userEvent.click(square)
+    setTimeout(() => {
+      expect(getComputedStyle(square).getPropertyValue("background-color")).toBe("green")
+    }, 100)
+  })
+  const square2 = screen.getByTestId("Player-cell-1-0")
+  expect(getComputedStyle(square2).getPropertyValue("background-color")).not.toBe("green")
+  act(() => {
+    userEvent.click(square2)
+    setTimeout(() => {
+      expect(getComputedStyle(square2).getPropertyValue("background-color")).toBe("green")
+    }, 100)
+  })
+  const square3 = screen.getByTestId("Player-cell-2-0")
+  expect(getComputedStyle(square3).getPropertyValue("background-color")).not.toBe("green")
+  act(() => {
+    userEvent.click(square3)
+    setTimeout(() => {
+      expect(getComputedStyle(square3).getPropertyValue("background-color")).toBe("green")
+    }, 100)
+  })
+  const square4 = screen.getByTestId("Player-cell-3-0")
+  expect(getComputedStyle(square4).getPropertyValue("background-color")).not.toBe("green")
+  act(() => {
+    userEvent.click(square4)
+    setTimeout(() => {
+      expect(getComputedStyle(square4).getPropertyValue("background-color")).toBe("green")
+    }, 100)
+  })
+  const square5 = screen.getByTestId("Player-cell-4-0")
+  expect(getComputedStyle(square5).getPropertyValue("background-color")).not.toBe("green")
+  act(() => {
+    userEvent.click(square5)
+    setTimeout(() => {
+      expect(getComputedStyle(square5).getPropertyValue("background-color")).not.toBe("green")
+    }, 100)
+  })
+  await waitFor(() => {
+    expect(playerInfo).not.toBeInTheDocument()
+  })
+
+})
+
 test("computer board renders", () => {
   render(<AppSection/>)
   const ComBoard = screen.getByTestId("Computer-board");
@@ -34,4 +83,3 @@ test("computer board renders", () => {
 })
 
 /* end react tests */
-
